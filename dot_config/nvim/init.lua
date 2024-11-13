7d742f65c728ae3671b8e8e3c33a2f0e65f36e8e
lost1fany <lost.fany@gmail.com>
2024-11-13 20:05:37 +0000

    Remove .config/nvim/.luarc.json Remove .config/nvim/init.lua Remove .config/nvim/lazy-lock.json Remove .config/nvim/lua/config/core.lua Remove .config/nvim/lua/config/functions.lua Remove .config/nvim/lua/config/keymap.lua Remove .config/nvim/lua/config/lazy.lua Remove .config/nvim/lua/plugins/auto-save.lua Remove .config/nvim/lua/plugins/autopairs.lua Remove .config/nvim/lua/plugins/bufferline.lua Remove .config/nvim/lua/plugins/catppuccin.lua Remove .config/nvim/lua/plugins/chatgpt.lua Remove .config/nvim/lua/plugins/completions.lua Remove .config/nvim/lua/plugins/conform.lua Remove .config/nvim/lua/plugins/copilot.lua Remove .config/nvim/lua/plugins/dashboard.lua Remove .config/nvim/lua/plugins/debugging.lua Remove .config/nvim/lua/plugins/lspconfig.lua Remove .config/nvim/lua/plugins/lualines.lua Remove .config/nvim/lua/plugins/mason-lspconfig.lua Remove .config/nvim/lua/plugins/mason.lua Remove .config/nvim/lua/plugins/mini-icons.lua Remove .config/nvim/lua/plugins/neo-tree.lua Remove .config/nvim/lua/plugins/neotest.lua Remove .config/nvim/lua/plugins/nvim-comment.lua Remove .config/nvim/lua/plugins/nvim-tmux-navigation.lua Remove .config/nvim/lua/plugins/oil.lua Remove .config/nvim/lua/plugins/swagger-preview.lua Remove .config/nvim/lua/plugins/telescope-ui-select.lua Remove .config/nvim/lua/plugins/telescope.lua Remove .config/nvim/lua/plugins/toggleterm.lua Remove .config/nvim/lua/plugins/treesitter.lua Remove .config/nvim/lua/plugins/undotree.lua Remove .config/nvim/lua/plugins/whichkeys.lua Remove .config/nvim/lua/plugins/yazi.lua

--- dot_config/nvim/init.lua
+++
@@ -1,4 +0,0 @@
-require("config.lazy")
-require("config.keymap")
-require("config.core")
-require("config.functions")
--- dot_config/nvim/lua/plugins/copilot.lua
+++
@@ -1,7 +0,0 @@
--- return {'github/copilot.vim'}
-
-return {
-    -- First run `:Codeium Auth`
-    'Exafunction/codeium.vim',
-    event = 'BufEnter'
-}
