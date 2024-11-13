9273ae0cbe5c8b565014e25776f5daeb09d4a7de
lost1fany <lost.fany@gmail.com>
2024-11-13 20:31:08 +0000

    Add .config/zsh/fzf.zsh

---
+++ dot_config/zsh/fzf.zsh
@@ -0,0 +1,31 @@
+# Set up fzf key bindings and fuzzy completion
+
+
+show_file_or_dir_preview="if [ -d {} ]; then eza --tree --color=always {} | head -200; else bat -n --color=always --line-range :500 {}; fi"
+
+export FZF_COMPLETION_TRIGGER='**'
+export FZF_COMPLETION_OPTS='--tmux bottom,100%,50% --border --info=inline'
+export FZF_CTRL_T_OPTS="--preview '$show_file_or_dir_preview'"
+
+_fzf_compgen_path() {
+  fd --hidden --follow --exclude ".git" . "$1"
+}
+
+_fzf_compgen_dir() {
+  fd --type d --hidden --follow --exclude ".git" . "$1"
+}
+
+
+_fzf_comprun() {
+  local command=$1
+  shift
+
+  case "$command" in
+    cd)           fzf --preview 'eza --tree --color=always {} | head -200' "$@" ;;
+    export|unset) fzf --preview "eval 'echo ${}'"         "$@" ;;
+    ssh)          fzf --preview 'dig {}'                   "$@" ;;
+    *)            fzf --preview "$show_file_or_dir_preview" "$@" ;;
+  esac
+}
+
+eval "$(fzf --zsh)"
