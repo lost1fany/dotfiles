08e321f8d054563a280a52281c31996c8fa15c5e
lost1fany <lost.fany@gmail.com>
2024-11-16 14:00:38 +0000

    Update .config/nvim/lua/config/core.lua

--- private_dot_config/nvim/lua/config/core.lua
+++ private_dot_config/nvim/lua/config/core.lua
@@ -40,16 +40,3 @@ vim.opt.fillchars = {
 }
 vim.cmd([[highlight clear LineNr]])
 vim.cmd([[highlight clear SignColumn]])
-
--- Filetypes
-vim.filetype.add({
-	extension = {
-		["sh.tmpl"] = "sh",
-		["toml.tmpl"] = "toml",
-		["yaml.tmpl"] = "yaml",
-		["yml.tmpl"] = "yaml",
-		["json.tmpl"] = "json",
-		["lua.tmpl"] = "lua",
-		["conf.tmpl"] = "conf",
-	},
-})
