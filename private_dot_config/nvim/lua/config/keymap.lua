6f797e5ee4cabaec9e07ea7c2d412e70d5e36720
lost1fany <lost.fany@gmail.com>
2024-11-16 20:49:00 +0000

    Update .config/nvim/lua/config/keymap.lua

--- private_dot_config/nvim/lua/config/keymap.lua
+++ private_dot_config/nvim/lua/config/keymap.lua
@@ -8,6 +8,9 @@ vim.keymap.set("i", "jj", "<Esc>", { silent = true })
 vim.keymap.set("i", "<Tab>", function()
 	return vim.fn["codeium#Accept"]()
 end, { expr = true, silent = true })
+vim.keymap.set("i", "<C-l>", function()
+    return vim.fn["codeium#AcceptLine"]()
+end, { expr = true, silent = true }
 vim.keymap.set("i", "<M-]>", function()
 	return vim.fn["codeium#CycleCompletions"](1)
 end, { expr = true, silent = true })
@@ -17,6 +20,7 @@ end, { expr = true, silent = true })
 vim.keymap.set("i", "<C-x>", function()
 	return vim.fn["codeium#Clear"]()
 end, { expr = true, silent = true })
+
 
 -- UndoTree
 vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { silent = true })
