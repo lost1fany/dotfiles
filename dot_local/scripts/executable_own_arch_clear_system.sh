84beaa8f0ac4ca677e0c09e77e3f071bdc2839c8
lost1fany <lost.fany@gmail.com>
2024-11-18 14:17:04 +0000

    Add .local/scripts/own_all_install-pd-tools.sh Add .local/scripts/own_arch_clear_system.sh

---
+++ dot_local/scripts/executable_own_arch_clear_system.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Check if user is root
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run as root"
+    exit
+fi
+
+# Clear not used packages
+pacman -Rns $(pacman -Qdtq) --noconfirm
+
+# Clear cache
+pacman -Scc --noconfirm
+
+journalctl --vacuum-time=1m
+journalctl --vacuum-size=1M
+rm -rf /var/cache/*
+rm -rf /var/tmp/*
+rm -rf ~/.cache/*
+
+dd if=/dev/zero of=/zerofile bs=1M status=progress
+rm -f /zerofile
+sync
+
+echo "Run: qemu-img convert -O qcow2 -c original.qcow2 compressed.qcow2"
