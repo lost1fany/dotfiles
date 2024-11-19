2d079c359bd4b0a52f38cfb065f25c2dcc9a0a21
lost1fany <lost.fany@gmail.com>
2024-11-19 07:17:44 +0000

    Update .local/scripts/own_arch_clear_system.sh

--- dot_local/scripts/executable_own_arch_clear_system.sh
+++ dot_local/scripts/executable_own_arch_clear_system.sh
@@ -6,20 +6,36 @@ if [ "$EUID" -ne 0 ]; then
     exit
 fi
 
-# Clear not used packages
+echo "[ ] Clear unused packages"
 pacman -Rns $(pacman -Qdtq) --noconfirm
+echo "[+]Clearing done"
 
-# Clear cache
+echo "[ ] Clear cache"
 pacman -Scc --noconfirm
+echo "[+] Done"
 
+echo "[ ] Clear logs"
 journalctl --vacuum-time=1m
 journalctl --vacuum-size=1M
+echo "[+] Done"
+
+echo "[ ] Clear tmp"
+rm -rf /tmp/*
+rm -rf /var/log/*
 rm -rf /var/cache/*
 rm -rf /var/tmp/*
 rm -rf ~/.cache/*
+echo "[+] Done"
 
+echo "[ ] Clear disk space"
+echo "   Step 1: Creating empty file"
 dd if=/dev/zero of=/zerofile bs=1M status=progress
-rm -f /zerofile
+echo "   Step 2: Syncing file"
 sync
+echo "   Step 3: Removing file"
+rm -f /zerofile
+echo "   Step 4: Syncing file"
+sync
+echo "[+] Done"
 
 echo "Run: qemu-img convert -O qcow2 -c original.qcow2 compressed.qcow2"
