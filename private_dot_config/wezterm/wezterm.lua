e85dd5bae381106f661dfc58e614c519ea371820
lost1fany <lost.fany@gmail.com>
2024-12-02 06:54:11 +0000

    Update .config/wezterm/wezterm.lua

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -1,32 +1,25 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
 
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
+config = {
+	enable_tab_bar = false,
+	initial_cols = 228,
+	initial_rows = 60,
 
-config.font = wezterm.font("JetBrains Mono", { weight = "Bold" })
-config.font_size = 12
---
-config.enable_tab_bar = false
+	font = wezterm.font("JetBrains Mono", { weight = "Bold" }),
+	font_size = 12,
 
-config.window_padding = {
-    left = 20,
-    right = 20,
-    top = 20,
-    bottom = 20,
+	window_padding = {
+		left = 20,
+		right = 20,
+		top = 20,
+		bottom = 20,
+	},
+	window_background_opacity = 0.8,
+	macos_window_background_blur = 10,
+	color_scheme = "Catppuccin Macchiato",
+	window_decorations = "RESIZE",
 }
--- Window defaults size
-config.initial_cols = 228
-config.initial_rows = 60
 
-config.color_scheme = "Catppuccin Macchiato"
-
-config.window_decorations = "RESIZE"
-
-config.window_background_opacity = 0.8
-config.macos_window_background_blur = 10
-
--- and finally, return the configuration to wezterm
 return config
