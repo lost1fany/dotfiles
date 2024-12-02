938f8c45f3092b959b2930c90ddf9e6006194cf6
lost1fany <lost.fany@gmail.com>
2024-12-02 07:43:06 +0000

    Update .config/wezterm/wezterm.lua

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -2,29 +2,24 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config = {
-	enable_tab_bar = false,
-	initial_cols = 228,
-	initial_rows = 60,
+config.automatically_reload_config = true
+config.enable_tab_bar = false
+config.initial_cols = 228
+config.initial_rows = 60
 
-	font = wezterm.font("JetBrains Mono", { weight = "Bold" }),
-	font_size = 12,
-
-	window_padding = {
-		left = 20,
-		right = 20,
-		top = 20,
-		bottom = 20,
-	},
-	window_background_opacity = 0.8,
-	macos_window_background_blur = 10,
-	color_scheme = "Catppuccin Macchiato",
-	window_decorations = "RESIZE",
-	window_close_confirmation = "NeverPrompt",
-	window_frame = {
-		active_titlebar_bg = "#1f1f1f",
-		inactive_titlebar_bg = "#1f1f1f",
-	},
+config.font = wezterm.font("JetBrains Mono", { weight = "Bold" })
+config.font_size = 12
+config.window_padding = {
+	left = 20,
+	right = 20,
+	top = 20,
+	bottom = 20,
 }
+config.window_background_opacity = 0.8
+config.macos_window_background_blur = 10
+config.color_scheme = "Catppuccin Macchiato"
+config.window_decorations = "RESIZE"
+config.window_close_confirmation = "NeverPrompt"
+config.default_cursor_style = "BlinkingBar"
 
 return config
